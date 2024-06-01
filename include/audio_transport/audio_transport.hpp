716b4800a12edd3da59e5844959fa0545edcdefd
deleonerick1808 <deleonerick1808@gmail.com>
2024-06-01 17:15:39 +0200

    Add files via upload

---
+++ include/audio_transport/audio_transport.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <vector>
+#include <tuple>
+#include <map>
+
+#include "audio_transport/spectral.hpp"
+
+namespace audio_transport {
+
+struct spectral_mass {
+  size_t left_bin;
+  size_t right_bin;
+  size_t center_bin;
+  double mass;
+};
+
+std::vector<audio_transport::spectral::point> interpolate(
+    const std::vector<audio_transport::spectral::point> & left,
+    const std::vector<audio_transport::spectral::point> & right,
+    std::vector<double> & phases,
+    double window_size,
+    double interpolation_factor);
+
+std::vector<std::tuple<size_t, size_t, double>> transport_matrix(
+    const std::vector<spectral_mass> & left,
+    const std::vector<spectral_mass> & right);
+
+std::vector<spectral_mass> group_spectrum(
+    const std::vector<audio_transport::spectral::point> & spectrum);
+
+void place_mass(
+    const spectral_mass & mass,
+    int center_bin,
+    double scale,
+    double interpolated_freq,
+    double center_phase,
+    const std::vector<audio_transport::spectral::point> & input,
+    std::vector<audio_transport::spectral::point> & output,
+    double next_phase,
+    std::vector<double> & phases,
+    std::vector<double> & amplitudes);
+
+}
